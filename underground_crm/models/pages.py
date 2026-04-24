08df9332041066c5bbb4368e4130ad90e364d743
Owen Miller <owen9825@gmail.com>
2026-04-24 17:19:41 +1000

    Extending metadata for pages

--- underground_crm/models/pages.py
+++ underground_crm/models/pages.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.db import models
 from wagtail.models import Page
 from wagtail.fields import StreamField
 from wagtail.blocks import (
@@ -9,10 +11,76 @@ from wagtail.blocks import (
     ChoiceBlock,
 )
 from wagtail.images.blocks import ImageChooserBlock
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, ObjectList, TabbedInterface
+from wagtail.admin.forms import WagtailAdminPageForm
 
 
-class BasicPage(Page):
+class PageWithMetadataForm(WagtailAdminPageForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        self.fields["author"].queryset = User.objects.order_by(
+            "-is_admin", "-is_staff", "first_name", "last_name"
+        )
+
+
+class PageWithMetadata(Page):
+    """
+    Abstract base class for pages that carry Open Graph metadata.
+
+    Provides search_image and og_type fields, and a standard set of
+    promote_panels covering the OG properties shared by all concrete
+    page types that inherit from this class.
+    """
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    search_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    og_type = models.CharField(
+        max_length=50,
+        default="article",
+        help_text=(
+            "The Open Graph type for this page. "
+            "See https://ogp.me/#types for the full list of valid types."
+        ),
+    )
+
+    base_form_class = PageWithMetadataForm
+
+    promote_panels = [
+        FieldPanel("slug"),
+        FieldPanel("author", heading="Author"),
+        FieldPanel("seo_title", heading="og:title"),
+        FieldPanel("search_description", heading="og:description"),
+        FieldPanel("search_image", heading="og:image"),
+        FieldPanel("og_type", heading="og:type"),
+    ]
+
+    visibility_panels = [
+        FieldPanel("show_in_menus"),
+        FieldPanel("go_live_at", heading="Publication time"),
+        FieldPanel("expire_at", heading="Expiration time"),
+    ]
+
+    class Meta:
+        abstract = True
+
+
+class BasicPage(PageWithMetadata):
     """
     A general-purpose content page built on StreamField. Supports rich text,
     raw HTML, images, and blockquotes as composable blocks.
@@ -78,6 +146,12 @@ class BasicPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel("body"),
     ]
+
+    edit_handler = TabbedInterface([
+        ObjectList(content_panels, heading="Content"),
+        ObjectList(PageWithMetadata.promote_panels, heading="Metadata"),
+        ObjectList(PageWithMetadata.visibility_panels, heading="Visibility"),
+    ])
 
     class Meta:
         verbose_name = "Basic Page"
